10b6a6b791e22250be7e29855996d683a1454f3f
Ree Britt <reebritt@icloud.com>
2021-01-24 14:15:45 -0500

    set local storage on hour blocks

--- Assets/script.js
+++ Assets/script.js
@@ -3,10 +3,30 @@
 var today = moment().format("MMM Do YYYY"); 
 $("#currentDay").text(today);
 
+$(document).ready(function(){
+    $("#hour9 .form").val(localStorage.getItem("hour9"));
+    $("#hour10 .form").val(localStorage.getItem("hour10"));
+    $("#hour11 .form").val(localStorage.getItem("hour11"));
+    $("#hour12 .form").val(localStorage.getItem("hour12"));
+    $("#hour1 .form").val(localStorage.getItem("hour1"));
+    $("#hour2 .form").val(localStorage.getItem("hour2"));
+    $("#hour3 .form").val(localStorage.getItem("hour3"));
+    $("#hour4 .form").val(localStorage.getItem("hour4"));
+    $("#hour5 .form").val(localStorage.getItem("hour5"));
+
+    //add click event to save text in hour
+    $(".saveBtn").on("click", function (){
+        var input = $(this).siblings(".form").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, input);
+    })
+
+})
 //Setting timeblocks 
 //var timeBlock = $(".hour");
 //var now = parseInt(moment().format(format("H")));
 
+var current
 //time-block{
 //    moment().startOf('hour');
 //    moment().minutes(0).seconds(0).milliseconds(0)
