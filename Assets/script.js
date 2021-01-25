8d1429b549abf6fcd062c129deabd3a6fd248b0b
Ree Britt <reebritt@icloud.com>
2021-01-24 19:38:08 -0500

    corrected html to names for proper css styling

--- Assets/script.js
+++ Assets/script.js
@@ -1,33 +1,47 @@
 
 // Display current day on the workday scheduler header
-var today = moment().format("MMM Do YYYY"); 
+var today = moment().format("LLLL"); 
 $("#currentDay").text(today);
 
+//setting timeblocks 
 $(document).ready(function(){
-    $("#hour9 .form").val(localStorage.getItem("hour9"));
-    $("#hour10 .form").val(localStorage.getItem("hour10"));
-    $("#hour11 .form").val(localStorage.getItem("hour11"));
-    $("#hour12 .form").val(localStorage.getItem("hour12"));
-    $("#hour1 .form").val(localStorage.getItem("hour1"));
-    $("#hour2 .form").val(localStorage.getItem("hour2"));
-    $("#hour3 .form").val(localStorage.getItem("hour3"));
-    $("#hour4 .form").val(localStorage.getItem("hour4"));
-    $("#hour5 .form").val(localStorage.getItem("hour5"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
-    //add click event to save text in hour
+    //add click event to save text & hour
     $(".saveBtn").on("click", function (){
-        var input = $(this).siblings(".form").val();
+        var input = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
         localStorage.setItem(time, input);
     })
 
-})
-//Setting timeblocks 
-//var timeBlock = $(".hour");
-//var now = parseInt(moment().format(format("H")));
+    // add function for timeblock color. Checking hour past, present, future 
+    function hourColor () {
+        var now = moment().hour();
+        $(".time-block").each(function (){
+            var time = parseInt($(this).attr("id").split("hour")[1]);
+            if (time < now) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            } else if (time === now) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
 
-var current
-//time-block{
-//    moment().startOf('hour');
-//    moment().minutes(0).seconds(0).milliseconds(0)
-//}
+    }
+    hourColor ()
+});
